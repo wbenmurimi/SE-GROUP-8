efd7adfa5f467d89b21953d02158ce8c4bfc2def
Jude Norvor <Jude Norvor>
2015-11-13 21:48:14 +0000

    implementation of users, manufacturers, labs and inventory class

---
+++ model/lab.php
@@ -0,0 +1,79 @@
+<?php
+/**
+*@author Jude Norvor
+*@version 0.0.2
+*/
+include_once ("adb.php");
+
+class Labs extends adb{
+	/**
+     *@method constructor newLab() Constructor for the lab class
+     * @param no parameter
+     * @return bool
+     */
+	function newLab (){
+			
+	}
+ 	/**
+     **@method boolean addLab($number, $name) Adding new Lab to inventory
+     * @param String $number lab number
+     * @param String $name name of lab
+     * @return bool
+     */
+	function addLab($number, $name){
+	    $str_query="INSERT INTO lecture_halls (hall_number, hall_name )VALUES ('$number','$name')";
+		return $this->query($str_query2);
+	}
+	/**
+     **@method boolean viewLabs() viewing all available labs
+     * @param no parameter
+     * @return bool
+     */
+	function viewLabs(){
+		$str_query="SELECT * FROM lecture_halls";
+		return $this->query($str_query);
+	}
+
+	/**
+     **@method boolean deleteLab($number) delete lab by lab number
+     * @param String number lab number
+     * @return bool
+     */
+   function deleteLab($number){
+		$str_query = "DELETE FROM lecture_halls WHERE hall_number = '$number'" ;
+		
+		return $this->query($str_query);	
+		
+		}	
+    /**
+     * @method boolean updateLab($number, $newName) Editing Lab name at lab number
+     * @param String $number specific inventory item
+     * @param String $newName inventory item
+     * @return bool
+     **/	
+	function updateLab($number, $newName){
+		$str_query = " UPDATE lecture_halls SET hall_name = '$newName' WHERE hall_number = '$number'";
+		return $this->query($str_query);
+    
+	}
+    /**
+     * @method boolean viewALab($no) Search for a lab in database
+     * @return bool
+     **/		
+	function viewALab($no){
+			$str_query="SELECT * FROM lecture_halls WHERE hall_number='$no'";
+			return $this->query($str_query);
+		}
+    /**
+     * @method boolean searchLab($searchItem) Search for labs at each column in database
+     * @return bool
+     **/	
+	function searchLab($searchLab){
+		
+		$str_query="SELECT * FROM lecture_halls WHERE hall_number LIKE '%$searchLab%' OR hall_name LIKE '%$searchLab%'";
+	
+		return $this->query($str_query);
+	}
+}
+	
+	?>
---
+++ model/manufacturer.php
@@ -0,0 +1,78 @@
+<?php 
+/**
+*@author Jude Norvor
+*@version 0.0.2
+*/
+include_once ("adb.php");
+
+class manufacturer extends adb {
+    /**
+     * @method boolean addManufacturer($id,$name,$phone,$email) Adding Manufacturer to database
+     * @param Int $id specific manufacturer id
+     * @param String $name name of manufacturer of inventory item
+     * @param Int $phone phone number of manufacturer
+     * @param Int $email email of manufacturer
+     * @return bool
+     **/	
+	function addManufacturer($id, $name,$phone,$email){
+		$str_query ="INSERT INTO manufacturer(manufacturer_id,manufacturer_name,phone_number, email) 
+        VALUES($id,'$name',$phone, '$email')";
+		return $this->query($str_query);
+	
+	}
+	/**
+     * @method boolean editManufacturer($id,$name,$phone,$email) Editing manufacturer information
+     * @param Int $id specific manufacturer id
+     * @param String $name name of manufacturer of inventory item
+     * @param Int $phone phone number of manufacturer
+     * @param Int $email email of manufacturer
+     * @return bool
+     **/
+	function editManufacturer($id,$name,$phone,$email){
+	
+	$str_query = "UPDATE manufacturer SET manufacturer_name='$name',
+	phone_number=$phone,email='$email'WHERE manufacturer_id=$id";
+
+		
+		return $this->query($str_query);
+	}
+    /**
+     * @method boolean deleteManufacturer($id) Deleting specific manufacturer
+     * @param Int id manufacturer id to be deleted
+     * @return bool
+     **/	
+	function deleteManufacturer($id) {
+	$str_query = " DELETE FROM manufacturer WHERE manufacturer_id = $id";
+	return $this->query($str_query);
+	}
+    /**
+     * @method boolean viewManufacturers() Showing all manufacturers
+     * @param no parameter
+     * @return bool
+     **/	
+	function viewManufacturers() {
+	$str_query = "SELECT * FROM manufacturer";
+	return $this->query($str_query);
+	}
+    /**
+     * @method boolean  viewAManufacturer($id) Search for a manufacturer in database
+     * @return bool
+     **/			
+	function viewAManufacturer($id) {
+	$str_query = "SELECT * FROM manufacturer WHERE manufacturer_id=$id";
+	return $this->query($str_query);
+	}
+	/**
+     * @method boolean searchManufacturer($searchManufacturer) Search for manufacturers at each column in database
+     * @return bool
+     **/	
+	function searchManufacturer($searchManufacturer){
+		
+		$querry="SELECT * FROM manufacturer WHERE manufacturer_id LIKE '%$searchManufacturer%' OR manufacturer_name LIKE '%$searchManufacturer%'
+			OR code_no LIKE '%$searchManufacturer%'";
+	
+		return $this->query($querry);
+	}
+	}
+
+?>	

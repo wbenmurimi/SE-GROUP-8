db7eeccf3888b4008841148986972c0f3ff8811a
Jude Norvor <Jude Norvor>
2015-11-14 16:04:07 +0000

    updated database

--- model/Database/inventory_management .sql
+++ model/Database/inventory_management .sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 13, 2015 at 08:17 PM
+-- Generation Time: Nov 14, 2015 at 05:01 PM
 -- Server version: 5.6.26
 -- PHP Version: 5.6.12
 
@@ -27,6 +27,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `items` (
+  `database_id` int(11) NOT NULL,
   `item_number` varchar(255) NOT NULL,
   `barcode_number` int(11) NOT NULL DEFAULT '0',
   `item_name` varchar(50) NOT NULL,
@@ -37,21 +38,22 @@ CREATE TABLE IF NOT EXISTS `items` (
   `conditions` varchar(100) NOT NULL,
   `location` varchar(50) NOT NULL,
   `department` varchar(50) NOT NULL,
-  `userid` int(11) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `userid` int(11) NOT NULL DEFAULT '0',
+  `quantity` int(5) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `items`
 --
 
-INSERT INTO `items` (`item_number`, `barcode_number`, `item_name`, `manufacturer`, `price`, `date_bought`, `last_repair_date`, `conditions`, `location`, `department`, `userid`) VALUES
-('3500', 2147483647, 'web Server', 'Samsung', 15000, '2014-04-20', '2014-03-14', 'Excellent', 'LAB 304', 'Electrical Engineering', 0),
-('4365', 2363, 'tool kits', 'Sonny', 1234, '1970-01-01', '1970-01-01', '', 'LH221', 'Computer Science', 0),
-('456', 3724640, '34545', 'HP', 547, '1970-01-01', '1970-01-01', 'bad', 'library', 'computer science', 0),
-('56432', 4089, 'internet cables', 'Sonny', 200, '1970-01-01', '1970-01-01', 'Good', 'Lab', 'Management Info Systems', 0),
-('5698756', 45694, 'Clasic laptop 2', 'Lenovo', 4500, '2015-10-02', '2014-02-02', 'fair', 'lab', 'Computer Engineering', 0),
-('600', 3489757, 'DELALI-', 'Samsung', 5000, '2012-04-12', '2014-08-10', 'poor', 'LAB221', 'Electrical Engineering', 0),
-('111', 222, 'Test', 'Samsung', 333, '0000-00-00', '0000-00-00', 'Testing', 'Accra', 'CS', 0);
+INSERT INTO `items` (`database_id`, `item_number`, `barcode_number`, `item_name`, `manufacturer`, `price`, `date_bought`, `last_repair_date`, `conditions`, `location`, `department`, `userid`, `quantity`) VALUES
+(1, '3500', 2147483647, 'web Server', 'Samsung', 15000, '2014-04-20', '2014-03-14', 'Excellent', 'LAB 304', 'Electrical Engineering', 0, 0),
+(2, '4365', 2363, 'tool kits', 'Sonny', 1234, '1970-01-01', '1970-01-01', '', 'LH221', 'Computer Science', 0, 0),
+(3, '456', 3724640, '34545', 'HP', 547, '1970-01-01', '1970-01-01', 'bad', 'library', 'computer science', 0, 0),
+(4, '56432', 4089, 'internet cables', 'Sonny', 200, '1970-01-01', '1970-01-01', 'Good', 'Lab', 'Management Info Systems', 0, 0),
+(5, '5698756', 45694, 'Clasic laptop 2', 'Lenovo', 4500, '2015-10-02', '2014-02-02', 'fair', 'lab', 'Computer Engineering', 0, 0),
+(6, '600', 3489757, 'DELALI-', 'Samsung', 5000, '2012-04-12', '2014-08-10', 'poor', 'LAB221', 'Electrical Engineering', 0, 0),
+(7, '111', 222, 'Test', 'Samsung', 333, '0000-00-00', '0000-00-00', 'Testing', 'Accra', 'CS', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -60,20 +62,20 @@ INSERT INTO `items` (`item_number`, `barcode_number`, `item_name`, `manufacturer
 --
 
 CREATE TABLE IF NOT EXISTS `lecture_halls` (
+  `id` int(11) NOT NULL,
   `hall_number` varchar(100) NOT NULL DEFAULT '0',
-  `hall_name` varchar(255) NOT NULL,
-  `item_no` int(11) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `hall_name` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `lecture_halls`
 --
 
-INSERT INTO `lecture_halls` (`hall_number`, `hall_name`, `item_no`) VALUES
-('100', 'Library', 0),
-('200', 'LIB', 0),
-('LAB 221', 'My Lab', 0),
-('LAB 402', 'EEngineering Lab', 0);
+INSERT INTO `lecture_halls` (`id`, `hall_number`, `hall_name`) VALUES
+(1, '100', 'Library'),
+(2, '200', 'LIB'),
+(3, 'LAB 221', 'My Lab'),
+(4, 'LAB 402', 'EEngineering Lab');
 
 -- --------------------------------------------------------
 
@@ -82,21 +84,24 @@ INSERT INTO `lecture_halls` (`hall_number`, `hall_name`, `item_no`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `manufacturer` (
+  `unique_id` int(11) NOT NULL,
   `manufacturer_id` int(11) NOT NULL DEFAULT '0',
   `manufacturer_name` varchar(255) NOT NULL,
-  `code_no` int(11) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `phone_number` int(12) NOT NULL DEFAULT '0',
+  `email` varchar(50) NOT NULL,
+  `address` varchar(100) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `manufacturer`
 --
 
-INSERT INTO `manufacturer` (`manufacturer_id`, `manufacturer_name`, `code_no`) VALUES
-(346, 'Ashesi', 46784),
-(400, 'Sony', 2350423),
-(999, 'Samsung', 34567),
-(2000, 'lenovo', 2),
-(364534, 'HP 23', 235472);
+INSERT INTO `manufacturer` (`unique_id`, `manufacturer_id`, `manufacturer_name`, `phone_number`, `email`, `address`) VALUES
+(1, 346, 'Ashesi', 46784, '', ''),
+(2, 400, 'Sony', 2350423, '', ''),
+(3, 999, 'Samsung', 34567, '', ''),
+(4, 2000, 'lenovo', 2, '', ''),
+(5, 364534, 'HP 23', 235472, '', '');
 
 -- --------------------------------------------------------
 
@@ -128,6 +133,24 @@ INSERT INTO `xx_user` (`user_id`, `user_name`, `user_pass`, `user_email`, `user_
 --
 
 --
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`database_id`);
+
+--
+-- Indexes for table `lecture_halls`
+--
+ALTER TABLE `lecture_halls`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `manufacturer`
+--
+ALTER TABLE `manufacturer`
+  ADD PRIMARY KEY (`unique_id`);
+
+--
 -- Indexes for table `xx_user`
 --
 ALTER TABLE `xx_user`
@@ -137,6 +160,21 @@ ALTER TABLE `xx_user`
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+  MODIFY `database_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `lecture_halls`
+--
+ALTER TABLE `lecture_halls`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `manufacturer`
+--
+ALTER TABLE `manufacturer`
+  MODIFY `unique_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `xx_user`
 --
